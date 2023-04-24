c8873b8dc46d6420e1664a4902daf5bf5f849688
Kirill <ganaboss66@gmail.com>
2023-04-24 12:02:51 +0300

    2

---
+++ Task_66/Program.cs
@@ -0,0 +1,13 @@
+﻿int Num(int m, int n)
+{
+   if (m == n)
+        return m;
+    return n + Num(m, n - 1);
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Num(m, n));
