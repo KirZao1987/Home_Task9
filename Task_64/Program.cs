1c76c1b80197a24d250df982a1214daadccab1d7
Kirill <ganaboss66@gmail.com>
2023-04-24 11:59:23 +0300

    1

---
+++ Task_64/Program.cs
@@ -0,0 +1,12 @@
+﻿string Num(int n)
+{
+    if (n == 1)
+        return "1";
+    return $"{n} " + Num(n - 1);
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Num(n));
